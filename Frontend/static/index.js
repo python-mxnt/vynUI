f58dde34cc5f9e84e5ad97976de264d81de84e44
mxntisbadatkotlin <verxateam@gmail.com>
2025-10-24 18:40:48 +0100

    MAJOR frontend update

--- Frontend/static/index.js
+++ Frontend/static/index.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".toggle-sidebar");
+  const sidebar = document.getElementById("sidebar");
+
+  toggleBtn.addEventListener("click", function () {
+    sidebar.classList.toggle("collapsed");
+  });
+});
+
 const block3button = document.getElementsByClassName("block3button");
 block3button.onclick = function viewusage() {
   window.open("../templates/usage.html", "_self");
